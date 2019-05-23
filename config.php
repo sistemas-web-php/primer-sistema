ffdcfb62f543b718da3be27f97045a1247d57f9e
Tiago <tiagoriver33@gmail.com>
2019-05-23 12:37:59 -0300

    estructura y archivos base

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+//constates de la conexion a la base de datos
+
+define("HOST", "localhost");
+define("USER", "root");
+define("DB", "bolsa_de_trabajo");
+
+//constantes de rutas
+
+define("RAIZ", $_SERVER['DOCUMENT_ROOT'] . "/bolsa_de_trabajo/");
+define("CONTROLADOR", $_SERVER['DOCUMENT_ROOT'] . "/bolsa_de_trabajo/controlador/");
+define("MODELO", $_SERVER['DOCUMENT_ROOT'] . "/bolsa_de_trabajo/modelo/");
+define("VISTA", $_SERVER['DOCUMENT_ROOT'] . "/bolsa_de_trabajo/vista/");
+define("WEB", "localhost/bolsa_de_trabajo");
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . "/bolsa_de_trabajo/config.php");
+// al ejecutarse siempre el index todo lo que se incluya aca se puede usar en todos lados.
+
+if (isset($_GET['view'])) {
+    include_once 'controlador/' . $_GET['view'] . '-controlador.php';
+} else {
+    include_once 'controlador/principal-controlador.php';
+}
