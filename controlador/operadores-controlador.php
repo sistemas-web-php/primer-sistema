1db4c9933d314dc58df6779b8895401a614946bf
Rodrigo Gabriel Reynoso <50190273+rodrigo402@users.noreply.github.com>
2019-05-28 23:42:38 -0300

    sdasd

--- controlador/operadores-controlador.php
+++ controlador/operadores-controlador.php
@@ -1,4 +1,5 @@
 <?php 
   include_once(VISTA . 'operadores-vista.php');
   include_once(MODELO . 'operador-modelo.php');
+  
 ?>
--- vista/footer.php
+++ vista/footer.php
@@ -5,10 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title></title>
-
-
-
-
     <link rel="stylesheet" href="<?php echo WEB . '/vista/css/bootstrap.css'; ?>">
     <link rel="stylesheet" href="<?php echo WEB . '/vista/css/mis-estilos.css'; ?>">
 
