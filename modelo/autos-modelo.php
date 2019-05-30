d0a9eb91269cf4dedd6c84e321ddde582a65e432
Tiago <tiagoriver33@gmail.com>
2019-05-30 14:05:15 -0300

    agrego la tabla de autos

--- modelo/autos-modelo.php
+++ modelo/autos-modelo.php
@@ -6,11 +6,11 @@ class auto
 {
 
     private $id;
-    private $auto;
+    private $marca;
     private $modelo;
     private $patente;
-    private $movil;
-    private $ingreso;
+    private $año_auto;
+    private $visibilidad;
 
     private $con;
 
@@ -18,30 +18,39 @@ class auto
         $conexion = new conexion();
         $this->con = $conexion->getCon();
     }
+    
     public function getId(){
-        return $this->id;
-    }
-
-    public function setId($id){
-        $this->id = $id;
-    }
-    public function getAuto(){
-		return $this->direccion;
+		return $this->id;
 	}
 
-	public function setAuto($auto){
-		$this->auto = $auto;
+	public function setId($id){
+		$this->id = $id;
     }
+    
+    public function getVisibilidad(){
+		return $this->id;
+	}
 
-    public function getModelo(){
+	public function setVisibilidad($visibilidad){
+		$this->visibilidad = $visibilidad;
+	}
+
+	public function getMarca(){
+		return $this->marca;
+	}
+
+	public function setMarca($marca){
+		$this->marca = $marca;
+	}
+
+	public function getModelo(){
 		return $this->modelo;
 	}
 
 	public function setModelo($modelo){
 		$this->modelo = $modelo;
-    }
-    
-   
+	}
+
 	public function getPatente(){
 		return $this->patente;
 	}
@@ -50,22 +59,14 @@ class auto
 		$this->patente = $patente;
 	}
 
-	public function getMovil(){
-		return $this->movil;
+	public function getAño_auto(){
+		return $this->año_auto;
 	}
 
-	public function setMovil($movil){
-		$this->movil = $movil;
+	public function setAño_auto($año_auto){
+		$this->año_auto = $año_auto;
 	}
 
-    public function getFecha_ingreso(){
-		return $this->fecha_ingreso;
-	}
-
-	public function setFecha_ingreso($fecha_ingreso){
-		$this->fecha_ingreso = $fecha_ingreso;
-    }
-    
     public function nuevoAuto()
     {
         try {
@@ -131,11 +132,10 @@ class auto
                 throw new Exception("Error al conectar a la base de datos");
             }else{
 
-                $sql = "SELECT id_auto, auto, modelo, patente, movil, fecha_ingreso_auto 
-                FROM autos";
+                $sql = "SELECT * FROM autos";
 
                 $resultado = mysqli_query($this->con, $sql);
-                
+
                 if($resultado){
 
                     $array_autos = array();
@@ -145,20 +145,20 @@ class auto
                         $auto = new auto();
 
                         $auto->setId($lista['id_auto']);
-                        $auto->setNombre($lista['auto']);
-                        $auto->setApellido($lista['modelo']);
-                        $auto->setDni($lista['patente']);
-                        $auto->setTelefono($lista['movil']);
-                        $auto->setFecha_ingreso($lista['fecha_ingreso_auto']);
+                        $auto->setMarca($lista['marca_auto']);
+                        $auto->setModelo($lista['modelo_auto']);
+                        $auto->setPatente($lista['patente_auto']);
+                        $auto->setAño_auto($lista['anio_auto']);
+                        $auto->setVisibilidad($lista['visibilidad_auto']);
 
-                        $array_auto[] = $auto;
+                        $array_autos[] = $auto;
 
                     }
 
-                    return $array_auto;
+                    return $array_autos;
 
                 } else {
-                    throw new Exception("al realizar la consulta");
+                    throw new Exception("error al realizar la consulta");
                 }
                 
             }
--- vista/autos-vista.php
+++ vista/autos-vista.php
@@ -17,17 +17,15 @@ input{
         <div id="mostrar" class='row'>
 
             <form action="nuevo_auto.php" method="POST" class='col-8 offset-2 row d-flex align-items-center'>
-              <label class='col-2'>Auto:</label>
-              <input type="text" required  name="auto"   placeholder="Ingrese nombre del auto" class="col-9 form-control">
+              <label class='col-2'>Marca:</label>
+              <input type="text" required  name="Marca"   placeholder="Ingrese marca del auto" class="col-9 form-control">
               <label class='col-2'>Modelo:</label>
               <input type="text"  required name="modelo" placeholder="Ingrese el modelo" class="col-9 form-control">
               <label class='col-2'>Patente:</label>
               <input type="text"  required name="patente" placeholder="Ingrese patente" class="col-9 form-control">
-              <label class='col-2'>N° de movil:</label>
+              <label class='col-2'>Año:</label>
               <input type="text"  required name="movil"placeholder="N° de movil" class="col-9 form-control">
-               <label class='col-2'>Ingreso:</label>
-              <input type="text" required name="ingreso" placeholder="Fecha de Ingreso" class="col-9 form-control"><br>
-              
+               
               <center><input type='submit' class="btn btn-secondary" value='Registrar'onclick="ocultar();"></center>
               
             </form>
@@ -35,14 +33,42 @@ input{
   <table class="table">
     <thead class="thead-light">
       <tr>
-        <th scope="col">AUTO</th>
-        <th scope="col">MODELO</th>
-        <th scope="col">PATENTE</th>
-        <th scope="col">N° MOVIL</th>
-        <th scope="col">INGRESO</th>
+        <th scope="col">Marca</th>
+        <th scope="col">Modelo</th>
+        <th scope="col">Patente</th>
+        <th scope="col">Año</th>
+        <th scope="col">visibilidad</th>
       </tr>
     </thead>
-  </table>
+  <?php 
+  if (count($array_autos) == 0) {
+    echo '</table> 
+          no hay ningun auto registrado en el sistema';
+  } else {
+
+    for ($i=0; $i < count($array_autos); $i++) { 
+
+      if ($array_autos[$i]->getVisibilidad()) {
+        $visibilidad = 'activo';
+        $color = "bg-success";
+      } else {
+        $visibilidad = 'no activo';
+        $color = "bg-danger";
+      }
+
+      echo 
+        "<tr>
+        <th>" . $array_autos[$i]->getMarca() . "</th>
+        <th>" . $array_autos[$i]->getModelo() . "</th>
+        <th>" . $array_autos[$i]->getPatente() . "</th>
+        <th>" . $array_autos[$i]->getAño_auto() . "</th> 
+        <th class='$color'>" . $visibilidad . "</th>  
+        </tr>";
+    }
+    
+  }
+  
+  ?>
 </body>
 <?php  include_once(VISTA . "footer.php"); ?>
 </html>
