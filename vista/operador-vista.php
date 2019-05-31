c72b5750f54aafad0e1461e5ece2358830bffe26
Tiago <tiagoriver33@gmail.com>
2019-05-31 11:56:04 -0300

    arreglo archivos borrados

---
+++ vista/operador-vista.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include_once(VISTA . "head.php"); ?>
+
+<body>
+
+<?php include_once(VISTA . "menu.php"); ?>
+
+<?php  include_once(VISTA . "footer.php"); ?>
+
+</body>
+
+</html>
